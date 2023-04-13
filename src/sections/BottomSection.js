77fa854874142feb89ce7d0aec7070d2d684118b
allenallada <allenallada58@gmail.com>
2023-04-14 00:39:56 +0800

    bottom section navigate

--- src/sections/BottomSection.js
+++ src/sections/BottomSection.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Box, Typography, Divider, Link } from '@mui/material';
-import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { SocialIcon } from 'react-social-icons';
 
 const socials = [
